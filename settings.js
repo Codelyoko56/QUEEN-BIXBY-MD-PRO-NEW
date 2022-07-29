2227987e9fc90b780bb5c3a3f84a6f570168a12a
Lyoko Bootz <109773394+Codelyoko56@users.noreply.github.com>
2022-07-29 18:31:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
+global.owner = ['94778479127']
+global.premium = ['9477847927']
+global.ownernomer = '9477847927'
 global.ownername = 'Dark Alpha'
 global.botname = 'BIXBY-MD USER BOT'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
